0365c0e2439ec9ec972e416f3092fe26076da2e6
Camillo <camrossi@cisco.com>
2021-10-14 15:19:44 +1100

    moved visibility to dedicated repo

--- visibility/build_push.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t quay.io/camillo/vkaci-init --target=vkaci-init  . &&  docker build -t quay.io/camillo/vkaci --target=vkaci .  &&  docker push quay.io/camillo/vkaci:latest &&  docker push quay.io/camillo/vkaci-init:latest

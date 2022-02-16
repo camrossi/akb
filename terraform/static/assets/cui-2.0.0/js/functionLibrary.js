c85e683c593ebe02751070f6d86b4df34cb2300c
Raj Kumar <kraj3@cisco.com>
2022-02-16 21:24:54 +1100

    added textarea saving, moved loading and saving to loading file,  made loading and saving dynamic, and added input saving to vcenter-login.html

---
+++ terraform/static/assets/cui-2.0.0/js/functionLibrary.js
@@ -0,0 +1,34 @@
+const tags = [["input", "text"], ["textarea"]];
+function saveInput() {
+  var inputs, index;
+  
+    for (const tag of tags) {
+        inputs = document.getElementsByTagName(tag[0]);
+        for (index = 0; index < inputs.length; ++index) {
+          currentElem = inputs[index];
+          if (tag.length == 1 || currentElem.type === tag[1]) {
+            console.log(currentElem)
+            sessionStorage.setItem(currentElem.id, currentElem.value);
+          }
+        }
+  }
+  console.log("saved");
+}
+function loadInput() {
+    var inputs, index;
+    // console.log("load attempt")
+    for (const tag of tags) {
+      inputs = document.getElementsByTagName(tag[0]);
+      for (index = 0; index < inputs.length; ++index) {
+        currentElem = inputs[index];
+        if (tag.length == 1 || currentElem.type === tag[1]) {
+          console.log(currentElem)
+            if (sessionStorage.getItem(currentElem.id) !== null) {
+                currentElem.value = sessionStorage.getItem(currentElem.id);
+            }
+        }
+      }
+    }
+  console.log("loaded");
+}
+

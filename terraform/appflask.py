c3c1cb936c5b9865277d2861993bdf76c428d225
Shangxin Du <shdu@cisco.com>
2022-02-17 22:49:41 -0800

    handle differnt fabric type in url

--- terraform/appflask.py
+++ terraform/appflask.py
@@ -354,7 +354,6 @@ def create():
                                         cluster)
             except Exception as e:
                 print(e)
-                config = []
         else:
             config = json.dumps({
                 "error": "fabric_type is invalid, chosse between aci and vxlan_evpn"

a7bd04818439ce6452bc86754ada7d7c12e8932e
Camillo <camrossi@cisco.com>
2022-03-16 15:40:02 +1100

    testing

--- packer/aws_upload.sh
+++ packer/aws_upload.sh
@@ -1,14 +1,12 @@
 #!/bin/bash 
 export AWS_PAGER=""
-#compress the OVA
-pigz $1 
-KEY=$(basename $1.gz)
+
+KEY=$(basename $1)
 #Calculate MD5 for upload
-MD5=$(openssl md5 -binary $1.gz | base64)
+MD5=$(openssl md5 -binary $1 | base64)
 
 #Split
-#split -b 4096m test.csv.gz test.csv.gz.part-
-split -b 40m $1.gz $1.gz.part-
+split -b 4096m $1 $1.part-
 
 # Create MultiPart Upload
 UPLOAD_ID=$(aws s3api create-multipart-upload \
@@ -17,7 +15,7 @@ UPLOAD_ID=$(aws s3api create-multipart-upload \
     --metadata md5=$MD5 | jq -r .UploadId)
 
 C=1
-for PART in `/bin/ls $1.gz.part-*`
+for PART in `/bin/ls $1.part-*`
     do
         aws s3api upload-part \
             --bucket $2 \

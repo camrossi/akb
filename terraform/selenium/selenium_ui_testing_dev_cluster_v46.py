174db486039f4d001ddff6b7c422f8a704b0943e
Camillo <camrossi@cisco.com>
2022-03-21 13:14:37 +1100

    Closes #112, Closes #111

--- terraform/selenium/selenium_ui_testing_dev_cluster_v46.py
+++ terraform/selenium/selenium_ui_testing_dev_cluster_v46.py
@@ -84,7 +84,7 @@ elem.send_keys("192.168.35.0/24")
 # WAIT FOR THE vrf_name_list TO BE POPULATED WITH AT LEAST 2 ELEMENTs (The first one is just the palce holder)
 # THAT SHOULD BE ALL IT TAKES TO HAVE THE REST OF THE PAGE READY...
 try:
-    elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="vrf_name_list"]/option[2]')))
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="vrf_name_list"]/option[2]')))
 except ValueError as e:
     print("Loading took too much time!")
 
@@ -135,7 +135,7 @@ select.select_by_visible_text("STLD")
 
 #Wait for vCenter API to populate the page
 try:
-    elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="datastore_list"]/option[1]')))
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="datastore_list"]/option[1]')))
 except ValueError as e:
     print("Loading took too much time!")
 

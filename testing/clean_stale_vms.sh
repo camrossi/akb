f81b008b5398b1e99575f58da54a5371c49de6b7
camillo.rossi@gmail.com <camrossi@cisco.com>
2022-05-26 18:22:37 +1000

    test

--- testing/clean_stale_vms.sh
+++ testing/clean_stale_vms.sh
@@ -1,12 +1,13 @@
 export VALIDATE_CERTS="no"
 export GOVC_INSECURE="1"
+echo -n "Ensure nkt VMs are not present" 
 while read -r line; 
 do
     export GOVC_URL=$line
-    echo -n "Ensure nkt VMs are not present on: " 
-    echo $GOVC_URL | cut -d"@" -f3
     for VM in `govc find / -type m -name "gitaction-nkt-*"`
     do
-        govc vm.destroy $VM
+        echo -n "Deleting VM:"
+        echo $VM
+        govc -v=true vm.destroy $VM
     done
 done < /home/cisco/Coding/akb/testing/virtual_centers.inv

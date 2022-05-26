14ea8a316ccf787ff91052a3e39013ad62b2870b
camillo.rossi@gmail.com <camrossi@cisco.com>
2022-05-26 18:41:52 +1000

    test

--- testing/clean_stale_vms.sh
+++ testing/clean_stale_vms.sh
@@ -1,6 +1,6 @@
 export VALIDATE_CERTS="no"
 export GOVC_INSECURE="1"
-echo -n "Ensure nkt VMs are not present" 
+echo "Ensure nkt VMs are not present" 
 while read -r line; 
 do
     export GOVC_URL=$line
@@ -11,3 +11,4 @@ do
         govc vm.destroy $VM
     done
 done < /home/cisco/Coding/akb/testing/virtual_centers.inv
+### The last line in virtual_centers.inv MUST be an empty line or we won't read the last vCenter!!!!
